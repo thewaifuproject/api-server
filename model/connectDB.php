6d50d50246b96da4732a52c5f77f0af1207948d3
Albert <me@albert.sh>
2018-11-12 20:48:54 +0100

    Clean and remove unneeded code

--- model/connectDB.php
+++ model/connectDB.php
@@ -1,30 +1,12 @@
 <?php
 function connectDB(){
-$servidor = "localhost"; $usuari = "root"; $clau = "123456";
-try{
-$connexio = new PDO("mysql:host=$servidor;dbname=waifus;charset=UTF8", $usuari, $clau);
-$connexio->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}catch(PDOException $e){ echo "Error: " . $e->getMessage(); }
-return($connexio);
+	$servidor = "localhost"; $usuari = "root"; $clau = "123456";
+	try{
+		$connexio = new PDO("mysql:host=$servidor;dbname=waifus;charset=UTF8", $usuari, $clau);
+		$connexio->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	}catch(PDOException $e){
+		echo "Error: " . $e->getMessage();
+	}
+	return($connexio);
 }
-
-/*
-
-function connectDB()
-{
-    $databaseUser = 'tdiw-k1';
-    $databasePassword = 'B7UnQW9R';
-
-    $conexion=NULL;
-   try
-   {
-      $conexion=new PDO("mysql:host=localhost; dbname=tdiw-k1; charset=UTF8", $databaseUser,$databasePassword);
-      $conexion->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-      
-   }catch (PDOExeption $e)
-   {
-      echo "ERROR: " .$e->getMessage();
-   }
-   return $conexion;
-}*/
 ?> 
